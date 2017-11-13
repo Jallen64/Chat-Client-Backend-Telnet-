78562da6c2f7943a9bc8500530ac2eba0dcdfc24
Joshua Andrew Allen <allen408@data.cs.purdue.edu>
2017-11-13 17:49:37 -0500

    Lab10

--- IRCServer.cpp
+++ IRCServer.cpp
@@ -470,6 +470,17 @@ IRCServer::enterRoom(int fd, const char * user, const char * password, const cha
 IRCServer::leaveRoom(int fd, const char * user, const char * password, const char * args)
 {
 
+	string str(args);
+        string str2(user);
+
+        map<string, struct ROOM>::iterator it;
+
+        it=roomMap.find(str);
+        it->second.guestVec.erase(std::remove( it->second.guestVec.begin(),  it->second.guestVec.end(), str),  it->second.guestVec.end());
+
+        const char * msg =  "OK\r\n";
+        write(fd, msg, strlen(msg));
+        return;
 
 
 }
