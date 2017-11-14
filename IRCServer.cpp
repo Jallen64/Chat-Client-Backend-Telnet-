1b2c73ee93e8fb4a066959571ffe6cf7f1b73ab5
Joshua Andrew Allen <allen408@data.cs.purdue.edu>
2017-11-14 08:35:24 -0500

    Lab10

--- IRCServer.cpp
+++ IRCServer.cpp
@@ -482,10 +482,10 @@ IRCServer::getMessages(int fd, const char * user, const char * password, const c
 	int i;
         for( i =finalNumber; i< it->second.messeges.size() ; i++){
 
-		string num = to_string(i);
+		string num = std::to_string(i);
                 string s;
 
-                s= num+ " " + it->second.messeges[i] + "\r\n";
+                s= num + " " + it->second.messeges[i] + "\r\n";
 
                 const char *msg = s.c_str();
 
