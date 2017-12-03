09e75f05cc89f7b3b033a4bfeeb480812701d590
Joshua Andrew Allen <allen408@data.cs.purdue.edu>
2017-12-03 05:44:15 -0500

    Lab10

--- IRCServer.cpp
+++ IRCServer.cpp
@@ -300,14 +300,14 @@ IRCServer::printVec()
     for(i=0; i <userVec.size(); i++)
     {
 
-        cout << userVec[i] << " ";
+        cout << "This is the userVec: " << userVec[i] << " ";
     }
     cout <<endl;
     int j;
     for(j=0; j <passVec.size(); j++)
     {
 
-        cout << passVec[j] << " ";
+        cout << "This is the passVec: " <<  passVec[j] << " ";
     }
     cout <<endl;
 }
